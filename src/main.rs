d3ba80d43f5d74ee12310f083b2173a0e2f7c13e
Julian Andrews <jandrews@fusionbox.com>
2019-03-24 10:39:29 -0700

    Return Result from main.
    This way, failures will return a non-zero return code.
    
    Also, use map_err to do the error mapping.

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,28 @@
 mod args;
 mod lib;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     let program = &args[0];
     let opts = args::build_opts();
     let (filename, number, min_entropy, ngram_length, min_word_length) =
-        match args::parse_args(&opts, &args) {
-            Ok(parsed_args) => parsed_args,
-            Err(_) => {
-                args::print_usage(program, &opts);
-                return;
-            }
-        };
+        args::parse_args(&opts, &args).map_err(|error| {
+            args::print_usage(program, &opts);
+
+            error
+        })?;
 
     let passphrases =
-        match lib::gen_passphrases(filename, number, min_entropy, ngram_length, min_word_length) {
-            Ok(passphrases) => passphrases,
-            Err(e) => {
-                eprintln!("{}: {}", program, e.description());
-                return;
-            }
-        };
+        lib::gen_passphrases(filename, number, min_entropy, ngram_length, min_word_length)
+            .map_err(|error| {
+                eprintln!("{}: {}", program, error.description());
+
+                error
+            })?;
 
     for (passphrase, entropy) in passphrases {
         println!("{} <{}>", passphrase, entropy);
     }
+
+    Ok(())
 }
