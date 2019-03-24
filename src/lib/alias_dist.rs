ac85ffa9bf0f48061d3ba34bb28cd35d8dc8e30e
Julian Andrews <jandrews@fusionbox.com>
2019-03-24 11:16:06 -0700

    Tweak some AliasDistribution reference handling.

--- src/lib/alias_dist.rs
+++ src/lib/alias_dist.rs
@@ -50,7 +50,7 @@ impl AliasDistribution {
         let mut entropy = 0.0;
 
         let mut probability_table = Vec::with_capacity(size);
-        for weight in weights.iter() {
+        for weight in weights {
             let prob = weight / total;
             entropy -= prob * prob.log(2.0);
             probability_table.push(prob * (size as f64));
@@ -59,8 +59,8 @@ impl AliasDistribution {
         let mut alias_table: Vec<usize> = (0..size).collect();
         let mut overfull: Vec<usize> = Vec::new();
         let mut underfull: Vec<usize> = Vec::new();
-        for (i, prob) in probability_table.iter().enumerate() {
-            if *prob < 1.0 {
+        for (i, &prob) in probability_table.iter().enumerate() {
+            if prob < 1.0 {
                 underfull.push(i);
             } else {
                 overfull.push(i);
